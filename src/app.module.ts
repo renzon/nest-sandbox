373d966dfcd0b494e2dd6f02a54d6ae2a6a4257a
renzo <renzon@gmail.com>
2023-05-18 16:46:04 -0300

    Modularização do módulo para ficar auto contido

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsController } from './items/items.controller';
-import { ItemsService } from './items/items.service';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ItemsController],
-  providers: [AppService, ItemsService],
+  imports: [ItemsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
