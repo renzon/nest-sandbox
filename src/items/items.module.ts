6a5fa1b7c89b9220b4607f4155eb7fb1bc9590eb
renzo <renzon@gmail.com>
2023-05-22 11:09:25 -0300

    Criando e usando Middlware em nível de módulo

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
+import { LoggerMiddleware } from './middleware/item.middleware';
 
 @Module({
   imports: [],
   controllers: [ItemsController],
   providers: [ItemsService],
 })
-export class ItemsModule {}
+export class ItemsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('items');
+  }
+}
